b0818c0fddb2c90d3be152be8ba018622c382b85
Eduardo Ruiz <eduarbo@gmail.com>
2011-12-18 03:07:30 -0600

    updated install script

--- install.sh
+++ install.sh
@@ -7,12 +7,6 @@ ln -sf $dir/gitconfig $HOME/.gitconfig
 
 if [[ `uname` == "Darwin"  ]]; then
     echo "source $dir/bash_profile" >> "$HOME/.profile"
-    # To refresh bash
-    source $HOME/.profile
-    source $HOME/.profile
 elif [[ `uname` == "Linux"  ]]; then
     echo "source $dir/bash_profile" >> "$HOME/.bashrc"
-    # To refresh bash
-    source $HOME/.bashrc
-    source $HOME/.bashrc
 fi
