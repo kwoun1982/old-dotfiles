e489149d7e1155a2891aa5b691deaa0d364e75fe
Eduardo Ruiz <eduarbo@gmail.com>
2011-12-18 02:46:25 -0600

    updated install script to add support for Linux

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 dir=$HOME/.dotfiles
-echo "source $dir/bash_profile" >> "$HOME/.profile"
 
 # old gitconfig backup
 [[ -e $HOME/.gitconfig ]] && mv $HOME/.gitconfig $HOME/.gitconfig.old
 ln -sf $dir/gitconfig $HOME/.gitconfig
 
-# To refresh bash
-source $HOME/.profile
-source $HOME/.profile
+if [[ `uname` == "Darwin"  ]]; then
+    echo "source $dir/bash_profile" >> "$HOME/.profile"
+    # To refresh bash
+    source $HOME/.profile
+    source $HOME/.profile
+elif [[ `uname` == "Linux"  ]]; then
+    echo "source $dir/bash_profile" >> "$HOME/.bashrc"
+    # To refresh bash
+    source $HOME/.bashrc
+    source $HOME/.bashrc
+fi
