763c099c72908467faa8db62c09a6f6b385a0ce6
Eduardo Ruiz + Narciso Guillen <eduarbo+narciso@tangosource.com>
2012-01-12 15:58:11 -0600

    fixed link to the bashrc in linux

--- install.sh
+++ install.sh
@@ -3,10 +3,9 @@ dir=$HOME/.dotfiles
 
 # old gitconfig backup
 [[ -e $HOME/.gitconfig ]] && mv $HOME/.gitconfig $HOME/.gitconfig.old
-ln -sf $dir/gitconfig $HOME/.gitconfig
 
 if [[ `uname` == "Darwin"  ]]; then
     echo "source $dir/bash_profile" >> "$HOME/.profile"
 elif [[ `uname` == "Linux"  ]]; then
-    echo "source $dir/bash_profile" >> "$HOME/.bash_profile"
+    echo "source $dir/bash_profile" >> "$HOME/.bashrc"
 fi
